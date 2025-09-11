7cfd5f96aa85f61bde8e985d879604d3cb4a1c7b
Varsha Danduri <varshadanduri@gmail.com>
2025-09-11 10:59:18 -0400

    Ctypes works. Can access sectionizer methods

--- engine.py
+++ engine.py
@@ -1,6 +1,11 @@
-import ctypes
+import ctypes, os
+
+dll_path = os.path.abspath('sectionizer.dll')
+LIB = ctypes.CDLL(dll_path)
+
+def load(x, y):
+    LIB.load(x, y)
 
 
-lib = ctypes.CDLL('library.dll')
-print(lib.main)
+
 
--- sectionizer/sectionizer.cpp
+++ sectionizer/sectionizer.cpp
@@ -2,18 +2,15 @@
 #include <map>
 #include <string>
 #include <regex>
+#include <cstring>
 
 using std::cout;
 using std::endl;
 using std::map;
 using std::string;
+using std::strncpy;
 
-
-
-extern "C" {
-
-    int main() {
-    map<string, bool>DEFAULT_ATTRS = {
+map<string, bool>DEFAULT_ATTRS = {
         {"diagnoses_history", true},
         {"family_history", true},
         {"medication_history", true},
@@ -21,15 +18,16 @@ extern "C" {
         {"mh_history", true}
     };
 
-    class Sectionizer{
+
+class Sectionizer{
         public:
             string language = "en";
             char name[11] = "Bio-Medcpp";
             int max_section_length;
             int min_section_length;
-            string start_line_phrase;
+            string start_line_phrase = "";
             char start_line_char;
-            string end_line_phrase;
+            string end_line_phrase = "";
             char end_line_char;
             char match_attr[6] = "lower";
             char new_line_rule[14] = "r[\n\r]+[s]*$";
@@ -45,14 +43,14 @@ extern "C" {
             char end_line_char,
             char match_attr[6]
             ) {
-            language = language;
-            max_section_length = max_section_length;
-            min_section_length = min_section_length;
-            start_line_phrase = start_line_phrase;
-            start_line_char = start_line_char;
-            end_line_phrase = end_line_phrase;
-            end_line_char = end_line_char;
-            match_attr = match_attr;
+            this->language = language;
+            this->max_section_length = max_section_length;
+            this->min_section_length = min_section_length;
+            this->start_line_phrase = start_line_phrase;
+            this->start_line_char = start_line_char;
+            this->end_line_phrase = end_line_phrase;
+            this->end_line_char = end_line_char;
+            strncpy(this->match_attr, match_attr, 6);
             };
 
             //lineRule -> return new_line_rule
@@ -72,11 +70,25 @@ extern "C" {
 
             //matchAttr -> returns match_attr
             char matchAttr() {
-                return match_attr[6];
+                return *match_attr;
             };
     };
-    return 0;
-}
+
+
+extern "C" {
+
+    void load(string l, 
+        int max, 
+        int min, 
+        string slp, 
+        char slc, 
+        string elp, 
+        char elc, 
+        char ma[6]){
+
+    Sectionizer newSectionizer(l, max, min, slp, slc, elp, elc, ma);
+    }
+
 }
 
 
